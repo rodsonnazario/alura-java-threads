6526201e22879f492ffeb434ec276a5718e1fe9a
rodsonnazario <rodson.nazario@gmail.com>
2022-12-03 23:23:28 -0300

    buscando nome em diferentes arquivos

---
+++ src/br/com/alura/threads/buscaTextual/Principal.java
@@ -0,0 +1,17 @@
+package br.com.alura.threads.buscaTextual;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		String nome = "da";
+		
+		Thread threadAssinaturas1 = new Thread(new TarefaBuscaTextual("assinaturas1.txt", nome));
+		Thread threadAssinaturas2 = new Thread(new TarefaBuscaTextual("assinaturas2.txt", nome));
+		Thread threadAutores = new Thread(new TarefaBuscaTextual("autores.txt", nome));
+		
+		threadAssinaturas1.start();
+		threadAssinaturas2.start();
+		threadAutores.start();
+	}
+}
