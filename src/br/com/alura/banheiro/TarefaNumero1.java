9b1d701b597b9468b7dcf5125407cc3a925c39d3
rodso <rodso@Nazario>
2022-12-06 23:28:16 -0300

    refatoração de pacotes

---
+++ src/br/com/alura/banheiro/TarefaNumero1.java
@@ -0,0 +1,16 @@
+package br.com.alura.banheiro;
+
+public class TarefaNumero1 implements Runnable {
+	
+	private Banheiro banheiro;	
+
+	public TarefaNumero1(Banheiro banheiro) {
+		super();
+		this.banheiro = banheiro;
+	}
+
+	@Override
+	public void run() {
+		banheiro.fazNumero1();
+	}
+}
