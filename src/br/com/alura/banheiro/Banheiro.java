eb92570b678dfa60a1d1e9c96487adfdf5d74567
rodso <rodso@Nazario>
2022-12-11 09:41:12 -0300

    threads aguardando

--- src/br/com/alura/banheiro/Banheiro.java
+++ src/br/com/alura/banheiro/Banheiro.java
@@ -2,12 +2,17 @@ package br.com.alura.banheiro;
 
 public class Banheiro {
 	
+	private boolean isSujo = true;
+	
 	public void fazNumero1() {
 		String nome = Thread.currentThread().getName();
 		System.out.println(nome + " batendo na porta");
 		
 		synchronized (this) {
 			System.out.println(nome + " entrando no banheiro");
+			if (isSujo) {
+				esperaLaFora(nome);
+			}
 			System.out.println(nome + " fazendo numero 1");
 			try {
 				Thread.sleep(5000);
@@ -18,13 +23,16 @@ public class Banheiro {
 			System.out.println(nome + " saindo do banheiro");	
 		}
 	}
-	
+
 	public void fazNumero2() {
 		String nome = Thread.currentThread().getName();
 		System.out.println(nome + " batendo na porta");
 		
 		synchronized (this) {
 			System.out.println(nome + " entrando no banheiro");
+			if (isSujo) {
+				esperaLaFora(nome);
+			}
 			System.out.println(nome + " fazendo numero 2");
 			try {
 				Thread.sleep(10000);
@@ -33,6 +41,15 @@ public class Banheiro {
 			}
 			System.out.println(nome + " lavando as mãos");
 			System.out.println(nome + " saindo do banheiro");	
+		}
+	}
+	
+	private void esperaLaFora(String nome) {
+		System.out.println(nome + ", eca, banheiro está sujo");
+		try {
+			this.wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 }
