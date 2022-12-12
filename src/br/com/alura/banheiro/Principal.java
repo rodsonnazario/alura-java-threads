4718c6cc4c6f9d287818ed74ea935c364c2605e4
rodso <rodso@Nazario>
2022-12-11 21:59:38 -0300

    usando thread deamon para executar enquanto outras threads estão ativas

--- src/br/com/alura/banheiro/Principal.java
+++ src/br/com/alura/banheiro/Principal.java
@@ -8,14 +8,15 @@ public class Principal {
 
 		Thread convidado1 = new Thread(new TarefaNumero1(banheiro), "João");
 		Thread convidado2 = new Thread(new TarefaNumero2(banheiro), "Pedro");
-		Thread limpeza = new Thread(new TarefaLimpeza(banheiro), "Funcionário");
 		Thread convidado3 = new Thread(new TarefaNumero1(banheiro), "Maria");
 		Thread convidado4 = new Thread(new TarefaNumero2(banheiro), "Ana");
+		Thread limpeza = new Thread(new TarefaLimpeza(banheiro), "Funcionário");
+		limpeza.setDaemon(true);
 
 		convidado1.start();
 		convidado2.start();
-		limpeza.start();
 		convidado3.start();
 		convidado4.start();
+		limpeza.start();
 	}
 }
