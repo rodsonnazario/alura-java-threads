45e946d028fb8dfa9059583a2d9283ee4a2d172f
rodsonnazario <rodson.nazario@gmail.com>
2022-12-16 22:40:38 -0300

    usando thread pool

--- src/br/com/alura/clienteServidor/servidor/ServidorTarefa.java
+++ src/br/com/alura/clienteServidor/servidor/ServidorTarefa.java
@@ -2,6 +2,8 @@ package br.com.alura.clienteServidor.servidor;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServidorTarefa {
 
@@ -9,14 +11,14 @@ public class ServidorTarefa {
 
 		System.out.println("--- Iniciando servidor ---");
 		ServerSocket servidor = new ServerSocket(12345);
-
+		ExecutorService threadPool = Executors.newCachedThreadPool();
+		
 		while (true) {
 			Socket socket = servidor.accept();
 			System.out.println("Aceitando novo cliente na porta " + socket.getPort());
 
 			DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
-			Thread threadCliente = new Thread(distribuirTarefas);
-			threadCliente.start();
+			threadPool.execute(distribuirTarefas);			
 		}
 	}
 }
